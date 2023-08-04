1e24be527867d69a5571eee208b6b9bf4137f855
zzuliudi <104132657+zzuliudi@users.noreply.github.com>
2023-08-04 08:53:11 +0800

    Add files via upload
    做完第一页面数据

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,31 +7,55 @@ Page({
    */
   data: {
     bannerList: [],
-    recommendList: []
+    recommendList: [],
+    topList: [],
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: async function (options) {
     // this为当前页面的实例对象
-    let bannerListData = await request("banner", { type: 2 })
-    let recommendListData = await request("personalized", { limit: 10 })
+    let bannerListData = await request("banner", {
+      type: 2
+    })
+    let recommendListData = await request("personalized", {
+      limit: 10
+    })
     this.setData({
       bannerList: bannerListData.banners,
-      recommendList: recommendListData.result
+      recommendList: recommendListData.result,
     })
+    let idsIndex = [991319590, 2809513713, 2809577409, 5059633707, 5059642708]
+    let resultArr = []
+    idsIndex.forEach(async (item) => {
+      let topListData = await request("playlist/detail", {
+        id: item
+      })
+      let topListItem = {
+        name: topListData.playlist.name,
+        tracks: topListData.playlist.tracks.slice(0, 3)
+      }
+      resultArr.push(topListItem)
+      //不需要五次请求结束
+      this.setData({
+        topList: resultArr
+      })
+      // 需要等待五次请求结束之后才更新，页面会长时间白屏，用户体验差
+      // return  this.setData({
+      //   topList: resultArr
+      // })
+    })
+    console.log(resultArr);
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady() {
-  },
+  onReady() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow() {
-  },
+  onShow() {},
 
   /**
    * 生命周期函数--监听页面隐藏
